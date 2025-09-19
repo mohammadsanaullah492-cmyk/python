35aafa59bd0f52ebbffafa2e7cb0a2610d631142
mohammadsanaullah492-cmyk <mohammadsanaullah492@gmail.com>
2025-09-18 21:55:21 -0700

    Add files via upload

---
+++ sapna.py
@@ -0,0 +1,20 @@
+def name(a,b):
+    print(a+b)
+
+name("sapna","bhai")     
+
+
+
+def detail(name,age,city):
+    print(name,age,city)
+
+detail("sanaullah",20,"delhi")     
+
+
+
+def name(a,b):
+    print(a+b)
+     
+name("anshuman","yaduvanshi")
+
+
